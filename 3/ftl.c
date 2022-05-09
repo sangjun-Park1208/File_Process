fe936e69d4e6736f91a7819fab29d9885eba3085
Sangjun Park <sangjun1997@naver.com>
2022-05-09 23:04:14 +0900

    fixing

--- 3/ftl.c
+++ 3/ftl.c
@@ -8,8 +8,9 @@
 #include <time.h>
 #include "blkmap.h"
 
-AddrMapTable addrmaptbl;
+AddrMapTbl addrmaptbl;
 extern FILE *devicefp;
+int reserved_empty_blk = DATABLKS_PER_DEVICE;
 
 /****************  prototypes ****************/
 void ftl_open();
@@ -17,68 +18,64 @@ void ftl_write(int lsn, char *sectorbuf);
 void ftl_read(int lsn, char *sectorbuf);
 void print_block(int pbn);
 void print_addrmaptbl();
+int dd_read(int ppn, char* pagebuf);
+int dd_write(int ppn, char* pagebuf);
 
-//
-// flash memory를 처음 사용할 때 필요한 초기화 작업, 예를 들면 address mapping table에 대한
-// 초기화 등의 작업을 수행한다
-//
-void ftl_open()
-{
+void ftl_open(){
 	int i;
 
-	// initialize the address mapping table
 	for(i = 0; i < DATABLKS_PER_DEVICE; i++)
-	{
-		addrmaptbl.pbn[i] = -1;
-	}
+		addrmaptbl.pbn[i] = DATABLKS_PER_DEVICE - i;
 
-	//
-	// 추가적으로 필요한 작업이 있으면 수행하면 되고 없으면 안해도 무방함
-	//
 
 	return;
 }
 
-//
-// file system을 위한 FTL이 제공하는 write interface
-// 'sectorbuf'가 가리키는 메모리의 크기는 'SECTOR_SIZE'이며, 호출하는 쪽에서 미리 메모리를 할당받아야 함
-//
-void ftl_write(int lsn, char *sectorbuf);
-{
-#ifdef PRINT_FOR_DEBUG			// 필요 시 현재의 block mapping table을 출력해 볼 수 있음
-	print_addrmaptbl_info();
+void ftl_write(int lsn, char *sectorbuf){
+	int pbn = lsn / PAGES_PER_BLOCK;
+	int offset = lsn % PAGES_PER_BLOCK;
+	int ppn = addrmaptbl.pbn[pbn] * BLOCK_SIZE + offset;
+
+	char* pagebuf = (char*)malloc(PAGE_SIZE);
+	strcpy(pagebuf, sectorbuf);
+	
+	SpareData* spd;
+	spd = (SpareData*)malloc(SPARE_SIZE);
+	spd->lsn = lsn;
+	memset(spd->dummy, '\0', SPARE_SIZE-4);
+	memcpy(pagebuf+SECTOR_SIZE, spd, SPARE_SIZE);
+
+	printf("spd->lsn : %d\n", spd->lsn);
+	printf("addrmaptbl.pbn[%d] : %d\n", pbn, addrmaptbl.pbn[pbn]);
+	printf("pagebuf : \n%s\n", pagebuf);
+
+	dd_write(ppn, pagebuf);
+
+	char* tmp = (char*)malloc(PAGE_SIZE);
+	dd_read(ppn, tmp);
+	printf("%s\n", tmp);
+
+#ifdef PRINT_FOR_DEBUG			
+//	print_addrmaptbl();
+//	print_block(pbn);
+#endif
+	
+	return;
+}
+
+void ftl_read(int lsn, char *sectorbuf){
+	
+
+#ifdef PRINT_FOR_DEBUG
+//	int pbn = lsn / PAGES_PER_BLOCK;
+//	print_block(pbn);
+//	print_addrmaptbl();
 #endif
 
-	//
-	// block mapping 기법에서 overwrite가 발생하면 이를 해결하기 위해 반드시 하나의 empty block이
-	// 필요하며, 초기값은 flash memory에서 맨 마지막 block number를 사용함
-	// overwrite를 해결하고 난 후 당연히 reserved_empty_blk는 overwrite를 유발시킨 (invalid) block이 되어야 함
-	// 따라서 reserved_empty_blk는 고정되어 있는 것이 아니라 상황에 따라 계속 바뀔 수 있음
-	//
-	int reserved_empty_blk = DATABLKS_PER_DEVICE;
-
-
 	return;
 }
 
-//
-// file system을 위한 FTL이 제공하는 read interface
-// 'sectorbuf'가 가리키는 메모리의 크기는 'SECTOR_SIZE'이며, 호출하는 쪽에서 미리 메모리를 할당받아야 함
-// 
-void ftl_read(int lsn, char *sectorbuf);
-{
-#ifdef PRINT_FOR_DEBUG			// 필요 시 현재의 block mapping table을 출력해 볼 수 있음
-	print_addrmaptbl_info();
-#endif
-
-	return;
-}
-
-//
-// for debugging
-//
-void print_block(int pbn)
-{
+void print_block(int pbn){
 	char *pagebuf;
 	SpareData *sdata;
 	int i;
@@ -88,9 +85,8 @@ void print_block(int pbn)
 
 	printf("Physical Block Number: %d\n", pbn);
 
-	for(i = pbn*PAGES_PER_BLOCK; i < (pbn+1)*PAGES_PER_BLOCK; i++)
-	{
-		read(i, pagebuf);
+	for(i = pbn*PAGES_PER_BLOCK; i < (pbn+1)*PAGES_PER_BLOCK; i++){
+		dd_read(i, pagebuf);
 		memcpy(sdata, pagebuf+SECTOR_SIZE, SPARE_SIZE);
 		printf("\t   %5d-[%7d]\n", i, sdata->lsn);
 	}
@@ -101,19 +97,12 @@ void print_block(int pbn)
 	return;
 }
 
-//
-// for debugging
-//
-void print_addrmaptbl()
-{
+void print_addrmaptbl(void){
 	int i;
 
 	printf("Address Mapping Table: \n");
 	for(i = 0; i < DATABLKS_PER_DEVICE; i++)
-	{
 		if(addrmaptbl.pbn[i] >= 0)
-		{
 			printf("[%d %d]\n", i, addrmaptbl.pbn[i]);
-		}
-	}
+	return;
 }
